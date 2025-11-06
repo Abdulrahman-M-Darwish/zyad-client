c4745c903eb5636760d10ddba4218253f5777745
Abdulrahman-M-Darwish <winding192837456456@gmail.com>
2025-11-06 21:13:57 +0200

    fixes-cookies

--- src/store/api.ts
+++ src/store/api.ts
@@ -5,6 +5,14 @@ export const api = createApi({
 	baseQuery: fetchBaseQuery({
 		baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
 		credentials: "include",
+		async prepareHeaders(headers) {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const cookies = (await getCookies()) as any;
+			const cookiesText = Object.keys(cookies)
+				.map((cookie: string) => `${cookie}=${cookies[cookie]}`)
+				.join(";");
+			headers.set("cookies", cookiesText);
+		},
 	}),
 	reducerPath: "api",
 	tagTypes: ["user", "levels", "sections"],
