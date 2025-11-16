d80a7f35a7c8cb99f82e76b2d15197dce8dc8a89
Sameh Muhammed <samehznaty@gmail.com>
2025-11-16 02:04:09 +0200

    test deploy

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -48,49 +48,7 @@ const LoginForm: React.FC = () => {
 	};
 	return (
 		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-				{/* eslint-disable-next-line @typescript-eslint/no-explicit-any*/}
-				{error && <ErrorMessage message={(error as any)?.data?.message} />}
-				<FormField
-					control={form.control}
-					name="email"
-					disabled={isLoading}
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Email</FormLabel>
-							<FormControl>
-								<Input placeholder="Email" {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="password"
-					disabled={isLoading}
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Password</FormLabel>
-							<FormControl>
-								<Input type="password" placeholder="Password" {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<div>
-					<Link
-						href="/forgot-password"
-						className="text-blue-400 hover:underline"
-					>
-						Forgot Password?
-					</Link>
-				</div>
-				<Button disabled={isLoading} type="submit" className="w-full">
-					Log In
-				</Button>
-			</form>
+		
 		</Form>
 	);
 };
